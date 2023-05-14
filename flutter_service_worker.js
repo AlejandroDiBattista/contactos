93a465a6cd176f9c1115bffd31b62aab5cfef3f5
Alejandro Di Battista <alejandrodibattista@gmail.com>
2023-05-14 20:31:35 -0300

    Agregando archivos generados por flutter build web 2

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -25,9 +25,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "3e522b56a0f091e531ed3e9b8ed5d666
 "icons/casco.png": "e68b000ae6545428ec08d3405a39682d",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "353766e4ad993719f1415be3f5fd7bae",
-"/": "353766e4ad993719f1415be3f5fd7bae",
-"main.dart.js": "754c30e9657b5fbea8ca75e22108b421",
+"index.html": "d6356ae064a9404eab380202890f8eef",
+"/": "d6356ae064a9404eab380202890f8eef",
+"main.dart.js": "f86edcab3c237d20a1765feeae2d1e49",
 "manifest.json": "6ccbf175281b24ae8f1180679442b5cb",
 "version.json": "389a6e6e6f34312743ca5f36467c84b5"};
 // The application shell files that are downloaded before a service worker can
