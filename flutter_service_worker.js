05b87401eaeb0b6b1bdd61a727ac74cbb4eeec22
Alejandro Di Battista <alejandrodibattista@gmail.com>
2023-05-15 17:24:13 -0300

    Agregando archivos generados por flutter build web 2

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -24,11 +24,11 @@ const RESOURCES = {"assets/AssetManifest.bin": "3e522b56a0f091e531ed3e9b8ed5d666
 "icons/casco.png": "e68b000ae6545428ec08d3405a39682d",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "3569f261bfaf43d8bc296ba5eca5d3c8",
-"/": "3569f261bfaf43d8bc296ba5eca5d3c8",
-"main.dart.js": "7aad3fafb19c27bf71b0f7382f99c61f",
+"index.html": "edbe0d830f826a0d9498dac6f2a4d177",
+"/": "edbe0d830f826a0d9498dac6f2a4d177",
+"main.dart.js": "ab147ae4ccb8243422ab392649ce6e4c",
 "manifest.json": "6ccbf175281b24ae8f1180679442b5cb",
-"version.json": "389a6e6e6f34312743ca5f36467c84b5"};
+"version.json": "e54704bcec42ae8b07615cc80277aa5c"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
