4cbb83885e001c9c3300550b835c8a631b2306f3
Alejandro Di Battista <alejandrodibattista@gmail.com>
2023-05-15 00:21:44 -0300

    Agregando archivos generados por flutter build web 2

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -24,8 +24,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "3e522b56a0f091e531ed3e9b8ed5d666
 "icons/casco.png": "e68b000ae6545428ec08d3405a39682d",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "1ead5c9763189f947f5fa7578e27f11e",
-"/": "1ead5c9763189f947f5fa7578e27f11e",
+"index.html": "636813b7f33adb421ed2c025821da13b",
+"/": "636813b7f33adb421ed2c025821da13b",
 "main.dart.js": "9a571573fe9edac36980004dea1afc22",
 "manifest.json": "6ccbf175281b24ae8f1180679442b5cb",
 "version.json": "389a6e6e6f34312743ca5f36467c84b5"};
