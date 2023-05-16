edf8e028d0a676f4f5c6e449706782152fa43648
Alejandro Di Battista <alejandrodibattista@gmail.com>
2023-05-16 10:58:56 -0300

    Agregando archivos generados por flutter build web 2

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -6,7 +6,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {"assets/AssetManifest.bin": "3e522b56a0f091e531ed3e9b8ed5d666",
 "assets/AssetManifest.json": "5d7a3224af01b50e7607d663ece2961c",
 "assets/datos/escuelas.json": "e653989ce90b82797110a5dc99d0bd71",
-"assets/datos/votantes.json": "3e88c49572103b2534854fb7294c7b9c",
+"assets/datos/votantes.json": "ad0a6262b690d218b48c1f32e69c67d4",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "e7069dfd19b331be16bed984668fe080",
 "assets/NOTICES": "9b97f4ef7a2b254c0c3de5fc3d2d0ed4",
@@ -24,9 +24,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "3e522b56a0f091e531ed3e9b8ed5d666
 "icons/casco.png": "e68b000ae6545428ec08d3405a39682d",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "1da9bcaae126726961cf042996eeee80",
-"/": "1da9bcaae126726961cf042996eeee80",
-"main.dart.js": "930738d657dfa3dbb18edbb86af37ad9",
+"index.html": "1549862443740d676601c7a7a447132a",
+"/": "1549862443740d676601c7a7a447132a",
+"main.dart.js": "6ce0ad8ebd54c86e4ce3e6cf53f73100",
 "manifest.json": "6ccbf175281b24ae8f1180679442b5cb",
 "version.json": "e54704bcec42ae8b07615cc80277aa5c"};
 // The application shell files that are downloaded before a service worker can
