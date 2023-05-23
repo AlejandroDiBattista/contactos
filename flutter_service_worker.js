05162b905ffe96f983b5c5503cf1402291656616
Alejandro Di Battista <alejandrodibattista@gmail.com>
2023-05-23 11:42:27 -0300

    Agregando archivos generados por flutter build web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -30,9 +30,9 @@ const RESOURCES = {"android-chrome-192x192.png": "28a423d79b2fa826b8650890ae4cf2
 "icons/PunteoYB-192.png": "c49cc2e6e0f85a09e43b7c56be1284ee",
 "icons/PunteoYB-512.png": "c3ea7af7b307825bdebdb8a732659a16",
 "icons/PunteoYB.png": "56211c388b6a701e9f3b240e3eadb93a",
-"index.html": "7960f60829b94ae1686e30dcfe92c95f",
-"/": "7960f60829b94ae1686e30dcfe92c95f",
-"main.dart.js": "04c1d7a4bba69d8466bccf3bdb084e40",
+"index.html": "5a2644fdcad0278154af9a5302201aa1",
+"/": "5a2644fdcad0278154af9a5302201aa1",
+"main.dart.js": "7b7528c7c2d8060ea2f1684d49fc9df9",
 "manifest.json": "1b94de60db31ef11233cb09bde112c54",
 "version.json": "e54704bcec42ae8b07615cc80277aa5c"};
 // The application shell files that are downloaded before a service worker can
