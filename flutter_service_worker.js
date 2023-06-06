4302e965e2025f62aec30c65c1fe027886904591
Alejandro Di Battista <alejandrodibattista@gmail.com>
2023-06-06 18:29:35 -0300

    Agregando archivos generados por flutter build web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -12,7 +12,7 @@ const RESOURCES = {"android-chrome-192x192.png": "28a423d79b2fa826b8650890ae4cf2
 "assets/datos/escuelas.json": "25f454b1cd831c48b15345cdc99a0c9f",
 "assets/datos/fondo.jpg": "42e2d74c89041c460492256c0301af05",
 "assets/datos/fondo.png": "e07d31a03cc50c024bdfbe5984384535",
-"assets/datos/votantes.json": "11524085c11b61267f9b99071aef00c7",
+"assets/datos/votantes.json": "dcdf5ba3dd52374a5ee10d4b0b56b5ce",
 "assets/FontManifest.json": "bf9160a436828f947d5b0d9b026cb4c7",
 "assets/fonts/MaterialIcons-Regular.otf": "e7069dfd19b331be16bed984668fe080",
 "assets/NOTICES": "e0ef891d0da8905d676c539b5493991a",
@@ -32,9 +32,9 @@ const RESOURCES = {"android-chrome-192x192.png": "28a423d79b2fa826b8650890ae4cf2
 "icons/PunteoYB-192.png": "c49cc2e6e0f85a09e43b7c56be1284ee",
 "icons/PunteoYB-512.png": "c3ea7af7b307825bdebdb8a732659a16",
 "icons/PunteoYB.png": "56211c388b6a701e9f3b240e3eadb93a",
-"index.html": "b2990fc39a0207cb60c913e9b7017ed2",
-"/": "b2990fc39a0207cb60c913e9b7017ed2",
-"main.dart.js": "ce0384bdf58584c8059968fab9cd9186",
+"index.html": "54f31683ff70578f34eadcd1d83c9b8d",
+"/": "54f31683ff70578f34eadcd1d83c9b8d",
+"main.dart.js": "fe40da7b8c1c3a95232c98e56993ee47",
 "manifest.json": "1b94de60db31ef11233cb09bde112c54",
 "version.json": "e54704bcec42ae8b07615cc80277aa5c"};
 // The application shell files that are downloaded before a service worker can
