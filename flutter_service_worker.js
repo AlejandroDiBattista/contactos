148f427525720d952f4541b372a5ca57a71f60ee
Alejandro Di Battista <alejandrodibattista@gmail.com>
2023-06-19 19:26:44 -0300

    Agregando archivos generados por flutter build web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -35,8 +35,8 @@ const RESOURCES = {"android-chrome-192x192.png": "28a423d79b2fa826b8650890ae4cf2
 "icons/PunteoYB-192.png": "c49cc2e6e0f85a09e43b7c56be1284ee",
 "icons/PunteoYB-512.png": "c3ea7af7b307825bdebdb8a732659a16",
 "icons/PunteoYB.png": "56211c388b6a701e9f3b240e3eadb93a",
-"index.html": "94c0788d35701349460a68d4fda9dffe",
-"/": "94c0788d35701349460a68d4fda9dffe",
+"index.html": "40f763d6dd93032458aeed707f0b2ecc",
+"/": "40f763d6dd93032458aeed707f0b2ecc",
 "main.dart.js": "8b7b6666229b320636fcd93a4ad7fbc8",
 "manifest.json": "1b94de60db31ef11233cb09bde112c54",
 "version.json": "e54704bcec42ae8b07615cc80277aa5c"};
